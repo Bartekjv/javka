40bbcfca3c9a752e4e3ca2aa4088b05a8ba88bc2
Bartekjv <barglowa16@gmail.com>
2019-02-14 14:21:23 +0100

    14.21

---
+++ src/jp1/Variables.java
@@ -0,0 +1,12 @@
+package jp1;
+
+public class Variables {
+    public static void main(String[] args) {
+        int x = 0;
+        System.out.println(x);
+        int y = x+11;
+        System.out.println("y= " +y);
+
+
+    }
+}
